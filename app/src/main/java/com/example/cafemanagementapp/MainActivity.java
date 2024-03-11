75dd743bbc24598e8416c91d92a2cd0a53949885
caominhnghia <huunghia9a122@gmail.com>
2024-03-11 23:22:18 +0700

    test commit

--- app/src/main/java/com/example/cafemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/cafemanagementapp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String test = "20200274-Nghia";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
